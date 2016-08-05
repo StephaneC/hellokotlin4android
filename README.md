242d4873817c07185e95c4486ce7f5ee62e0f715
StephaneC <Stephane.castrec@gmail.com>
2016-08-05 22:11:18 +0200

    Hello works

---
+++ README.md
@@ -0,0 +1,14 @@
+# HelloKotlin4Android
+
+## Install
+User Android studio and install plugin 
+[Plugin](https://blog.jetbrains.com/kotlin/2013/08/working-with-kotlin-in-android-studio/)
+
+## Configure project
+### Configure gradle
+See both build.gradle
+### Converting Java code to Kotlin
+Use action "Convert Java F to Kotlin File"
+### Configuring Kotlin in the project
+Use action "Configure Kotlin in Project"
+Now the build.gradle should be updated
--- app/src/main/java/stephane/castrec/hellokotlin4android/MainActivity.kt
+++ app/src/main/java/stephane/castrec/hellokotlin4android/MainActivity.kt
@@ -1,12 +1,45 @@
 package stephane.castrec.hellokotlin4android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    //private var  editTxt: EditText?
+    //private var  txt: TextView?
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //No need because Kotlin extension plugin
+        // txt = findViewById(R.id.txt) as TextView?;
+        // editTxt = findViewById(R.id.edittxt) as EditText?;
+
+        findViewById(R.id.btn_snack).setOnClickListener {
+            snack();
+        }
+        findViewById(R.id.btn_toast).setOnClickListener {
+            toast();
+        }
+        findViewById(R.id.btn_txt).setOnClickListener {
+            textView();
+        }
+    }
+
+    fun snack(){
+        Snackbar.make(layout, edittxt.text.toString(), Snackbar.LENGTH_LONG).show()
+    }
+
+    fun textView(){
+        txt.setText(edittxt.text.toString())
+    }
+
+    fun toast(){
+        Toast.makeText(this, edittxt.text.toString(), Toast.LENGTH_LONG).show()
     }
 }
